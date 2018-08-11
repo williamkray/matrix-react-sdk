f0c3aff50eebafa3b1315c2ce031162cef14609e
Sorunome <mail@sorunome.de>
2018-08-11 12:46:23 +0200

    fix license headers

--- src/Tinter.js
+++ src/Tinter.js
@@ -1,6 +1,7 @@
 /*
 Copyright 2015 OpenMarket Ltd
 Copyright 2017 New Vector Ltd
+Copyright 2018 ponies.im
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,9 +14,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
 
-/*
 Additionally, original modifications by ponies.im are licensed under the CSL.
 See https://coinsh.red/csl/csl.txt or the provided CSL.txt for additional information.
 These modifications may only be redistributed and used within the terms of 
